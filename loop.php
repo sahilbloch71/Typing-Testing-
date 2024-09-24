23424ed7e34bd002590615cd799d928eb7aff814
Sahil <113378049+sahilbloch71@users.noreply.github.com>
2024-09-24 19:52:59 +0530

    loop

---
+++ loop.php
@@ -0,0 +1,13 @@
+<?php
+//Star Pyramid Size
+$size = 5;
+for($i=1;$i<=$size;$i++){
+    for($j=1;$j<=$size-$i;$j++){
+        echo "&nbsp;&nbsp;";
+    }
+    for($k=1;$k<=$i;$k++){
+                echo "*&nbsp;&nbsp;";
+    }
+echo "<br />";
+}
+?>
